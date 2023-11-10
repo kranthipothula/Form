e554c559a80aa750d7ca7e66cef10612c1a91725
kranthipothula <150351636+kranthipothula@users.noreply.github.com>
2023-11-10 13:32:13 +0530

    09/11/2023

---
+++ new/index.js
@@ -0,0 +1,7 @@
+import page from  "./page"
+
+export default function Home(){
+    return(
+        <validate/>
+    )
+}
---
+++ new/login form.js
@@ -0,0 +1,43 @@
+// pages/login.js
+import { useState } from 'react';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your authentication logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
